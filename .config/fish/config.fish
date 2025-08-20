675a8e9f1255ad7eddb3980a27e9aa6549698891
andreacaldato-hub <andreacaldato6@gmail.com>
2025-08-20 12:31:22 +0200

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,17 @@ end
 
 # Better prompt (using fish built-in)
 # Created by `pipx` on 2025-08-19 13:20:23
-set PATH $PATH /home/andrea/.local/bin
+# PATH
+set -gx PATH $HOME/.local/bin $PATH
+
+# Shortcuts
+
+# Git bare repo dotfiles config
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+# Useful
+alias update='sudo pacman -Syu'
+
+# Enable vi keybindings
+fish_vi_key_bindings
 neofetch
